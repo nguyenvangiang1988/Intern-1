7a4114acc1021727ca0ed005e4b770c0a9777aa6
hungtam <hungtamse@gmail.com>
2019-06-03 12:37:58 +0700

    add makefile and fix some format code

--- lhungtam/server.c
+++ lhungtam/server.c
@@ -19,10 +19,11 @@
 
 int client[10], sizeMaxClient=10;
 
-void clearClient(){
+void ClearClient(){
     int i;
-    for(i=0; i<sizeMaxClient; i++)
+    for (i=0; i<sizeMaxClient; i++){
         client[i]=0;
+    }
 }
 
 int main(){
@@ -43,13 +44,13 @@ int main(){
     char string[1024];
 
     valueOfSocket=socket(AF_INET, SOCK_STREAM, 0);
-    if(valueOfSocket<0){
+    if (valueOfSocket<0){
         printf("Error create server\n");
         return -1;
     }
-    else
+    else{
         printf("Create server successful\n");
-
+    }
     memset(&addresServer, '\0', sizeof(addresServer));
 
     addresServer.sin_port=htons(1234);
@@ -57,76 +58,93 @@ int main(){
     inet_aton("192.168.81.11", &addresServer.sin_addr.s_addr);
 
     valueOfBind=bind(valueOfSocket,(struct sockaddr*)&addresServer, sizeof(addresServer));
-    if(valueOfBind==0)
+    if (valueOfBind==0){
         printf("Bind is successful\n");
-    else{
+    }
+    else {
         printf("Error bind\n");
         perror("BIND: \n");
         goto CLOSE;
     }
+
     valueOfListen=listen(valueOfSocket, 3);
-    if(valueOfListen==0)
+    if (valueOfListen==0){
         printf("Listening...\n");
-    else{
+    }
+    else {
         printf("false\n");
         goto CLOSE;
     }
 
     int i;
-    clearClient();
+    ClearClient();
     while(1){
+
         FD_ZERO(&listSocket);
         FD_SET(valueOfSocket, &listSocket);
         sizeFD=valueOfSocket;
 
-        for(i=0; i<sizeMaxClient; i++){
-            if(client[i]!=0)
-                FD_SET(client[i], &listSocket);
+        for (i=0; i<sizeMaxClient; i++){
 
-            if(sizeFD < client[i])
+            if (client[i]!=0){
+                FD_SET(client[i], &listSocket);
+            }
+
+            if (sizeFD < client[i]){
                 sizeFD=client[i];
+            }
         }
+
         select(sizeFD+1, &listSocket, NULL, NULL, NULL);
 
-        if(FD_ISSET(valueOfSocket, &listSocket)){
+        if (FD_ISSET(valueOfSocket, &listSocket)){
+
             addressClient_Len=sizeof(struct sockaddr_in);
             valueOfAccept=accept(valueOfSocket, (struct sockaddr*)&addressClient, &addressClient_Len);
-            if(valueOfAccept > 0){
-                printf("Accept successful IP: %s, Socket=%d\n", inet_ntoa(addressClient.sin_addr),valueOfAccept);
+
+            if (valueOfAccept > 0){
+                printf("Accept successfully IP: %s, Socket=%d\n", inet_ntoa(addressClient.sin_addr),valueOfAccept);
                 int j;
-                for(j=0; j<sizeMaxClient; j++)
-                    if(client[j]==0){
+
+                for (j=0; j<sizeMaxClient; j++){
+                    if (client[j]==0){
                         client[j]=valueOfAccept;
                         break;
                     }
+                }
             }
             else{
                 printf("false\n");
-                return -1;
             }
         }
 
-        for(i=0; i<sizeMaxClient; i++)
-            if(FD_ISSET(client[i],&listSocket)){
+        for (i=0; i<sizeMaxClient; i++){
+
+            if (FD_ISSET(client[i],&listSocket)){
+
                 memset(&string, '\0', sizeof(string));
                 recv(client[i], string, sizeof(string), 0);
                 puts(string);
                 printf("socket=%d\n",client[i]);
-                if(strcmp(string, "EXIT")==0){
+
+                if (strcmp(string, "EXIT")==0){
                     client[i]=0;
                     close(client[i]);
                 }
             }
+        }
     }
 
     valueOfShutdown=shutdown(valueOfSocket, SHUT_RDWR);
-    if(valueOfShutdown==0)
+
+    if (valueOfShutdown==0){
         printf("Shutdown successful\n");
-    else
-        CLOSE:
-            close(valueOfSocket);
-            unlink(valueOfSocket);
-            return -1;
-    unlink(valueOfSocket);
+    }
+    else{
+        printf("shutdown false\n");
+    }
+    CLOSE:
+        close(valueOfSocket);
+        return -1;
     return 0;
 }
